8d2d2506e80a2a974386a9995f6eb6a5c32e514a
Manny Rodriguez <66538419+er-arcadio@users.noreply.github.com>
2022-09-08 09:36:28 -0400

    angelica refactor (#1)

--- testwillpower.py
+++ testwillpower.py
@@ -1,8 +1,8 @@
-import diet
+import willpower
 
 # Code out your tests here. Some sample tests for the first function have already been written.
 # Remember to use the dot syntax of "diet.functionName()" when referencing functions from the other file.
 
-print(diet.perfect_day(0))
+print(willpower.perfect_day(0))
 
-print(diet.perfect_day(1))
+print(willpower.perfect_day(1))
---
+++ willpower.py
@@ -0,0 +1,28 @@
+# Define your functions here
+
+# 1. Define a function that returns the string "Stop Bullying!" if Angelica has bullied any babies.
+#    If she has bullied zero babies, return the string "Good job, Angelica."
+
+def perfect_day(bullied_babies):
+   return
+
+# 2. Not every day can be a perfect day. Let's write a function for a "good_day" of no bullying.
+#    If Angelica has bullied fewer than three babies, it should return a string that congratulates her.
+#    If Angelica has bullied exactly three babies, let's return a string telling her to stop now.
+#    If Angelica has bullied more than three babies, let's return a string scolding her for bullying too many.
+
+
+
+# 3. The longer Angelica is consistently not bullying, the more we feel we can trust her. Let's write a flex_day function.
+#    Let's write a function that takes in two arguments - a goal number of bullied babies and current number of bullied babies.
+#    It will do the same as the previous function did, so calling flex_day(2, 3) should return a scolding string, since she ate one more than her goal.
+
+
+
+# 4. CHALLENGE: Being nice isn't only about not bullying naive babies, cutting out the bad; it's also about being actively nice to them.
+#    Let's define a function called balanced_bully that takes in two arguments - a number of bullied babies and a number of hugged babies.
+#    If Angelica has hugged more babies than she has bullied, congratulate her!
+#    If Angelica has the same amount for each, warn her that she should hug a baby next.
+#    If Angelica has more babies that she bullied rather than hugged, tell her to get back on track. 
+#    If Angelica has hugged some babies and has not bullied at all, give her a special congratulations.
+#    If Angelica has neither hugged nor bullied any babies, encourage her to do something - preferably hug a baby.
